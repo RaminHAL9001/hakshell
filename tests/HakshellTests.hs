a602cc0998aafa233446c8ac05048e595954720e
Ramin Honary <ramin.honary@gmail.com>
2019-03-21 01:15:47 +0900

    Started writing a test program

---
+++ tests/HakshellTests.hs
@@ -0,0 +1,37 @@
+module Main where
+
+import           Hakshell.String
+import           Hakshell.TextEditor
+
+import           Control.Monad.IO.Class
+
+----------------------------------------------------------------------------------------------------
+
+main :: IO ()
+main = newTextBuffer defaultTags >>= runEditText editTextTests >>= \ case
+  Left (TextEditError err) -> putStrLn $ unpack err
+  Right () -> return ()
+
+----------------------------------------------------------------------------------------------------
+
+type Tags = ()
+
+defaultTags :: Tags
+defaultTags = ()
+
+report :: MonadIO m => String -> m ()
+report = liftIO . putStr
+
+editTextTests :: EditText Tags ()
+editTextTests = do
+  let reportInsert str = do
+        report $ "insertString " ++ show str ++ "\n"
+        insertString str
+  reportInsert "one two three\n"
+  reportInsert "four five six\nseven eight nine\nten eleven twelve\n"
+  report "OK"
+  forLinesInBufferM (1::Int) $ \ _ line -> do
+    n <- state $ \ n -> (n, n+1)
+    liftIO $ putStrLn $ show n ++ ": " ++ unpack line
+    return [line]
+  return ()
