833e9e348ad52aad23830798a6898cf8c75fa1df
Ramin Honary <ramin.honary@gmail.com>
2019-05-11 21:45:58 +0900

    Renamed foreach->pipeEach, foreachP->mapToPipe, added foreach, pmap
    The new 'foreach' and 'pmap' act on 'Pipe's rather than lists. Also
    added a lot more comments.

--- src/Hakshell/Pipe.hs
+++ src/Hakshell/Pipe.hs
@@ -1,7 +1,7 @@
 -- | Combinators for building composable IO functions that share some of the behavior of UNIX
 -- pipelines. This is accomplished with a 'Pipe' data type. For the most part, you shouldn't need
--- these functions for day-to-day shell use cases. These combinators are used to defining new shell
--- function.
+-- these functions for day-to-day shell use cases, except perhaps for 'foreach' and 'pmap' which are
+-- generally very useful. These combinators are used to define new shell function.
 --
 -- Hakshell shell functions are of a type similar to this:
 --
@@ -24,8 +24,40 @@ import qualified Data.ByteString.UTF8  as UTF8
 
 type ErrMsg = UTF8.ByteString
 
--- | 'Pipe' is a Mealy Machine data type, meaning after pattern matching on a 'Pipe' to extract a
--- value, another IO evaluator is also extracted and then evaluated to produce another 'Pipe'.
+-- | 'Pipe' is a data type used to approximate the behavior of UNIX pipes.
+--
+-- You use the 'pull' function to evaluate an 'Applicative' function of your choice on the content
+-- within the 'Pipe'. The 'Pipe' is a "Mealy Machine" data type, meaning when 'pull' is evaluated,
+-- it pattern match on the 'Pipe' to extract a value to be applied to your 'Applicative' function,
+-- and then another applicative function is also extracted and evaluated to produce another 'Pipe',
+-- and each successive 'Pipe' is recursively evaluated.
+--
+-- Construct a 'Pipe' using functions like 'push' and 'foreach'. Pipe is also a functor, so you can
+-- modify the type of @a@ by evaluating a function on it with 'fmap'. 'Pipe' is not an 'Applicative'
+-- or a 'Monad', which makes it unlike a list. However like list, it does instantiate 'Semigroup'
+-- and 'Monoid', so if you want to sequence items evaluated by 'pull' within a 'Pipe' you can use
+-- the ('<>') operator to append more items onto the content stored within the 'Pipe'. For example:
+--
+-- @
+-- return ('push' 1 <> 'push' 2 <> 'push' 3) >>= pull (print . (+ 3))
+-- @
+--
+-- The above will, for each 'push'ed element, add 3 and then print the result.
+--
+-- Monoid/Semigroup appending is lazy, so you can append a 'PipeFail' value to a chain of
+-- non-failing ''Pipe's and evaluate 'pull' such that 'pull' stops evaluating before the failure is
+-- reached. This can be dnoe ifting a 'Control.Monad.Cont.ContT' and force the 'pull' evaluator to
+-- halt:
+--
+-- @
+-- 'Control.Monad.Cont.runContT'
+--     ('Control.Monad.Cont.callCC' $ \\ halt -> -- <- here we define the halt function
+--         return ('push' 1 <> 'push' 2 <> PipeFail "error!") >>=
+--             'pull' (\\ i -> if i > 1
+--                       then halt () -- halt when we get to 2, 'PipeFail' will not be evaluated.
+--                       else print (i + 3))
+--     ) return -- <-- tell 'ContT' to simply 'return' the last value returned or passed to halt.
+-- @
 data Pipe m a
   = PipeStop
   | PipeFail !ErrMsg
@@ -36,6 +68,16 @@ instance Functor m => Functor (Pipe m) where
     PipeStop        -> PipeStop
     PipeFail   msg  -> PipeFail msg
     PipeNext a next -> PipeNext (f a) $ fmap (fmap f) next
+
+instance Functor m => Monoid (Pipe m a) where
+  mempty = PipeStop
+  mappend a b = case a of
+    PipeNext a nextA -> case b of
+      PipeNext{} -> PipeNext a $ flip mappend b <$> nextA
+      PipeFail{} -> PipeNext a $ flip mappend b <$> nextA
+      PipeStop   -> PipeNext a nextA
+    PipeFail{} -> a
+    PipeStop   -> b
 
 -- | Yield a single value and then end.
 push :: Applicative m => a -> m (Pipe m a)
@@ -49,16 +91,26 @@ pushList = \ case
 
 -- | Similar to 'forM', but evaluates a function on each element of a list, and each item is
 -- 'yield'ed in turn.
-foreach :: Applicative m => [a] -> (a -> m b) -> m (Pipe m b)
-foreach ax f = case ax of
+pipeEach :: Applicative m => [a] -> (a -> m b) -> m (Pipe m b)
+pipeEach ax f = case ax of
   []   -> pure PipeStop
-  a:ax -> flip PipeNext (foreach ax f) <$> f a
+  a:ax -> flip PipeNext (pipeEach ax f) <$> f a
 
-foreachP :: Applicative m => Pipe m a -> (a -> m b) -> m (Pipe m b)
-foreachP ax f = case ax of
+-- | Same as 'foreach' but with the parameters flipped.
+mapToPipe :: Applicative m => (a -> m b) -> [a] -> m (Pipe m b)
+mapToPipe = flip pipeEach
+
+-- | Like 'pipeEach' but essentially maps a function to each element in the pipe using the ('<*>')
+-- operator.
+foreach :: Applicative m => Pipe m a -> (a -> m b) -> m (Pipe m b)
+foreach ax f = case ax of
   PipeStop        -> pure PipeStop
   PipeFail msg    -> pure $ PipeFail msg
-  PipeNext a next -> PipeNext <$> f a <*> (flip foreachP f <$> next)
+  PipeNext a next -> PipeNext <$> f a <*> (flip foreach f <$> next)
+
+-- | Same as 'foreach' but with the parameters flipped.
+pmap :: Applicative m => (a -> m b) -> Pipe m a -> m (Pipe m b)
+pmap = flip foreach
 
 -- | Pull values from the 'Pipe', apply each value to the given continuation. The value returned by
 -- continuation is ignored, this function is only for evaluating on functions that produce
