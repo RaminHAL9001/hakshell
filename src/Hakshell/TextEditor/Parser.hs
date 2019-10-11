51f5e31cf294ef1808736bb0ca5b5c408ca684d4
Ramin Honary <ramin.honary@gmail.com>
2019-10-11 16:01:25 +0900

    Cosmetic change: variable 'st' changed to 'fold' everywhere

--- src/Hakshell/TextEditor/Parser.hs
+++ src/Hakshell/TextEditor/Parser.hs
@@ -86,14 +86,14 @@ parserUserState = lens theParUserState $ \ a b -> a{ theParUserState = b }
 
 -- | This is the internal control structure for a 'Parser' function type. After evaluating a
 -- 'Parser' function, the result of parsing is expressed as a value of this type.
-data ParStep tags st m a
+data ParStep tags fold m a
   = Backtrack
   | ParSuccess a
-  | ParWaiting !(ParserState tags st) (Parser tags st m a)
-  | ParError   !(ParserState tags st) !StrictBytes
+  | ParWaiting !(ParserState tags fold) (Parser tags fold m a)
+  | ParError   !(ParserState tags fold) !StrictBytes
   deriving Functor
 
-instance Monad m => Applicative (ParStep tags st m) where
+instance Monad m => Applicative (ParStep tags fold m) where
   pure = ParSuccess
   a <*> b = case a of
     Backtrack                 -> Backtrack
@@ -105,11 +105,11 @@ instance Monad m => Applicative (ParStep tags st m) where
     ParWaiting st0 (Parser a) -> ParWaiting st0 $ Parser $ (<*> b) <$> a
     ParError   err  msg       -> ParError err msg
 
-instance Monad m => Alternative (ParStep tags st m) where
+instance Monad m => Alternative (ParStep tags fold m) where
   empty = Backtrack
   a <|> b = case a of { Backtrack -> b; a -> a; }
 
-instance Monad m => Monad (ParStep tags st m) where
+instance Monad m => Monad (ParStep tags fold m) where
   return = pure
   (>>=) = \ case
     Backtrack                 -> const Backtrack
@@ -117,7 +117,7 @@ instance Monad m => Monad (ParStep tags st m) where
     ParWaiting  st (Parser a) -> \ f -> ParWaiting st $ Parser $ (>>= f) <$> a
     ParError    err      msg  -> const $ ParError err msg
 
-instance Monad m => MonadPlus (ParStep tags st m) where { mzero = empty; mplus = (<|>); }
+instance Monad m => MonadPlus (ParStep tags fold m) where { mzero = empty; mplus = (<|>); }
 
 ----------------------------------------------------------------------------------------------------
 
@@ -126,19 +126,21 @@ instance Monad m => MonadPlus (ParStep tags st m) where { mzero = empty; mplus =
 -- instantiates the 'Parsing' typeclass, so you should define parsers using the 'Parsing' typeclass
 -- APIs, and then evaluate these parsers on a 'TextBuffer' to extract information about the text in
 -- the 'TextBuffer' using the 'execParser' function.
-newtype Parser tags st m a
-  = Parser{ unwrapParser :: StateT (ParserState tags st) (EditText tags m) (ParStep tags st m a) }
+newtype Parser tags fold m a
+  = Parser
+    { unwrapParser :: StateT (ParserState tags fold) (EditText tags m) (ParStep tags fold m a)
+    }
   deriving Functor
 
-instance Monad m => Applicative (Parser tags st m) where
+instance Monad m => Applicative (Parser tags fold m) where
   pure = Parser . return . pure
   (Parser a) <*> (Parser b) = Parser $ (<*>) <$> a <*> b
 
-instance Monad m => Alternative (Parser tags st m) where
+instance Monad m => Alternative (Parser tags fold m) where
   empty = Parser $ return empty
   (Parser a) <|> (Parser b) = Parser $ (<|>) <$> a <*> b
 
-instance Monad m => Monad (Parser tags st m) where
+instance Monad m => Monad (Parser tags fold m) where
   return = pure
   (Parser a) >>= f = Parser $ a >>= \ case
     Backtrack        -> return Backtrack
@@ -146,9 +148,9 @@ instance Monad m => Monad (Parser tags st m) where
     ParWaiting  st a -> return $ ParWaiting st $ a >>= f
     ParError err msg -> return $ ParError err msg
 
-instance Monad m => MonadPlus (Parser tags st m) where { mzero = empty; mplus = (<|>); }
+instance Monad m => MonadPlus (Parser tags fold m) where { mzero = empty; mplus = (<|>); }
 
-instance Monad m => MonadError StrictBytes (Parser tags st m) where
+instance Monad m => MonadError StrictBytes (Parser tags fold m) where
   throwError msg = Parser $ flip ParError msg <$> get
   catchError (Parser try) catch = Parser $ try >>= \ case
     Backtrack       -> return Backtrack
@@ -156,10 +158,10 @@ instance Monad m => MonadError StrictBytes (Parser tags st m) where
     ParWaiting st a -> return $ ParWaiting st $ catchError a catch
     ParError  _ msg -> unwrapParser $ catch msg
 
-instance Monad m => MonadFail (Parser tags st m) where
+instance Monad m => MonadFail (Parser tags fold m) where
   fail = throwError . pack
 
-instance Monad m => MonadState st (Parser tags st m) where
+instance Monad m => MonadState fold (Parser tags fold m) where
   state f = Parser $ state $ \ st ->
     let (a, ust) = f (st ^. parserUserState) in (ParSuccess a, st & parserUserState .~ ust)
 
@@ -169,14 +171,14 @@ newParserState
   :: (MonadIO m
      , Show tags --DEBUG
      )
-  => st -> EditText tags m (ParserState tags st)
-newParserState ust = do
+  => fold -> EditText tags m (ParserState tags fold)
+newParserState fold = do
   buf    <- currentBuffer
   stream <- newStreamCursor
   return ParserState
     { theParBuffer    = buf
     , theParStream    = stream
-    , theParUserState = ust
+    , theParUserState = fold
     }
 
 ---- | Evaluates a 'Parser' using an existing 'ParserState' that has already been constructed with a
@@ -212,7 +214,7 @@ thePosition :: ParserState tags fold -> TextLocation
 thePosition = streamLocation . theParStream
 
 -- | Pass this function to 'parserGet' to obtain the current line being inspected by the parser.
-theCurrentLine :: Monad m => Parser tags st m (TextLine tags)
+theCurrentLine :: Monad m => Parser tags fold m (TextLine tags)
 theCurrentLine = Parser $ ParSuccess . streamCache <$> use parserStream
 
 -- | This is a lens that can be used with 'parserModify' to alter the @tags@' for the current line.
@@ -221,5 +223,5 @@ currentTags = parserStream . streamTags
 
 ----------------------------------------------------------------------------------------------------
 
---instance Parsing (Parser tags st) where
+--instance Parsing (Parser tags fold) where
 --  try f = Parser get >>= \ st -> f <|> (Parser (put st) >> mzero)
